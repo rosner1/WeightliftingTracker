90b8c1981841e304f181c5614c8053f766d766d8
Jonah Rosner <rosner1@purdue.edu>
2026-01-06 23:04:59 -0500

    Initial commit

---
+++ src/main/java/jonah/WeightliftingTracker/WeightliftingTrackerApplication.java
@@ -0,0 +1,13 @@
+package jonah.WeightliftingTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightliftingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightliftingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jonah/WeightliftingTracker/WeightliftingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package jonah.WeightliftingTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightliftingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
